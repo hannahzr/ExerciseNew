99cc4273c034a7d14d84bfcee5096f6e26f26fb6
hannahzr <hannahzafirah07@gmail.com>
2026-06-22 09:36:36 +0800

    Create new file : index.php

--- index.php
+++ index.php
@@ -3,11 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello, World!</title>
+    <title>New Exercise</title>
 </head>
 <body>
-    <?php
-        echo "This is a simple PHP script that outputs 'Hello, World!' to the browser.";
-    ?>
+    
 </body>
 </html>
